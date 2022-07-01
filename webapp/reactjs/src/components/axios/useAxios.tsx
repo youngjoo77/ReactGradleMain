f3d975312e57ee9b33f7f1e0a3b734249caaab4e
youngjoo77 <youngjoo.lee@NB-20091105>
2022-07-01 14:50:44 +0900

    commit

--- webapp/reactjs/src/components/axios/useAxios.tsx
+++ webapp/reactjs/src/components/axios/useAxios.tsx
@@ -17,6 +17,14 @@ interface FetchData {
 };
 
 const fetchAuth = async (fetchData: FetchData) => {
+	/**
+       * 개발 환경에서의 크로스 도메인 이슈를 해결하기 위한 코드로
+       * 운영 환경에 배포할 경우에는 axios.defaults.baseURL,axios.defaults.withCredentials  주석 처리합니다.
+       * 
+       * ※크로스 도메인 이슈: 브라우저에서 다른 도메인으로 URL 요청을 하는 경우 나타나는 보안문제
+       */
+	axios.defaults.baseURL = 'http://localhost:8081';
+	axios.defaults.withCredentials = true; 
 	
 	const baseUrl = "/api";
 	const method = fetchData.method;
