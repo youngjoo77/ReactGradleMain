42a4481ee64463a35bf398dda54c15ffac15bbd7
youngjoo77 <youngjoo.lee@NB-20091105>
2022-07-01 14:19:18 +0900

    commit

--- src/main/java/com/react/test/config/WebSecurityConfig.java
+++ src/main/java/com/react/test/config/WebSecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.react.test.jwt.JwtAccessDeniedHandler;
 import com.react.test.jwt.JwtAuthenticationEntryPoint;
@@ -20,7 +22,7 @@ import lombok.RequiredArgsConstructor;
 @Configuration
 @EnableWebSecurity
 @Component
-public class WebSecurityConfig {
+public class WebSecurityConfig implements WebMvcConfigurer{
     private final TokenProvider tokenProvider;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
@@ -30,6 +32,15 @@ public class WebSecurityConfig {
     	// request로부터 받은 비밀번호를 암호화
         return new BCryptPasswordEncoder();
     }
+    
+    /*
+	 * 개발환경에서의 크로스 도메인 이슈 해결을 위한 코드. 운영 배포 시 addCorsMappings 내용 주석처리
+	 */
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/api/**").allowCredentials(true).allowedOrigins("http://localhost:3000");
+		WebMvcConfigurer.super.addCorsMappings(registry);
+	}
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -41,7 +52,7 @@ public class WebSecurityConfig {
         .authenticationEntryPoint(jwtAuthenticationEntryPoint)
         .accessDeniedHandler(jwtAccessDeniedHandler)
         
-        .and().authorizeRequests()
+        .and().authorizeRequests() 
         .antMatchers("/api/auth/**").permitAll() // 토큰 검증 권한설정
         .anyRequest().authenticated()
         
