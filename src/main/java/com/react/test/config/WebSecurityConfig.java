7f86868fbe22cf486082919d32625479a19b2baa
youngjoo77 <youngjoo.lee@NB-20091105>
2022-07-01 17:23:12 +0900

    화면 권한 설정

--- src/main/java/com/react/test/config/WebSecurityConfig.java
+++ src/main/java/com/react/test/config/WebSecurityConfig.java
@@ -1,16 +1,16 @@
 package com.react.test.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.react.test.jwt.JwtAccessDeniedHandler;
 import com.react.test.jwt.JwtAuthenticationEntryPoint;
@@ -22,10 +22,16 @@ import lombok.RequiredArgsConstructor;
 @Configuration
 @EnableWebSecurity
 @Component
-public class WebSecurityConfig implements WebMvcConfigurer{
+public class WebSecurityConfig {
     private final TokenProvider tokenProvider;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+    	// resource 하위로 권한을 다줘 버림.
+        return (web) -> web.ignoring().antMatchers("/static/**", "/favicon.ico", "/manifest.json", "/**");
+    }
     
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -35,8 +41,8 @@ public class WebSecurityConfig implements WebMvcConfigurer{
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.httpBasic().disable() // http 사용금지
-        http.csrf().disable() // react 에서 token을 localstorage에 저장 하기 위한 설정
+        http.httpBasic().disable() // http 사용금지
+        .csrf().disable() // react 에서 token을 localstorage에 저장 하기 위한 설정
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
        
         .and().exceptionHandling()
@@ -44,9 +50,23 @@ public class WebSecurityConfig implements WebMvcConfigurer{
         .accessDeniedHandler(jwtAccessDeniedHandler)
         
         .and().authorizeRequests()
+        
+        // static resources 권한 설정
+        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+        
+        .antMatchers("/").permitAll()
+        .antMatchers("/login").permitAll()
         .antMatchers("/api/auth/**").permitAll() // 토큰 검증 권한설정
         .anyRequest().authenticated()
         
+        .and()
+        .formLogin()
+        .loginPage("/login") // 로그인 페이지 링크
+        .defaultSuccessUrl("/main") // 로그인 성공 후 리다이렉트 주소
+        .and()
+        .logout()
+        .logoutSuccessUrl("/login") // 로그아웃 성공시 리다이렉트 주소
+    
         .and()
         .apply(new JwtSecurityConfig(tokenProvider));
 
