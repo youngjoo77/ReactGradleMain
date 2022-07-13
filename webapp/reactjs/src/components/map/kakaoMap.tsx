3aeae4b00f2a6e41e2b85b52e72e7bce29c1c141
youngjoo77 <youngjoo.lee@NB-20091105>
2022-07-13 10:24:10 +0900

    카카오맵 설정

--- webapp/reactjs/src/components/map/kakaoMap.tsx
+++ webapp/reactjs/src/components/map/kakaoMap.tsx
@@ -30,56 +30,73 @@ const KakaoMap = ({
 	longitude,
 	place
 }: KakaoMapProp) => {
-	const { kakao }  : any = window;
-	
+	const { kakao }: any = window;
+
 	const handleClose = () => {
 		setOpen(false);
+		setOpenHandler(false);
 	};
 
 
-//	const GenerateKakaoMap = () => {
-//		console.log("START");
-//		const mapScript = document.createElement("script");
-//		mapScript.async = true;
-////		mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAOMAP_APPKEY}&autoload=false`;
-//		mapScript.src = '//dapi.kakao.com/v2/maps/sdk.js?appkey=ab4a5501e93f5f214f48bc77557e3fd2&autoload=false';
-//
-//
-//		const onLoadKakaoMap = () => {
-//			kakao.maps.load(() => {
-//				const container = document.getElementById("kakaoMapArea");
-//				const options = {
-////					center: new window.kakao.maps.LatLng(Number(latitude), Number(longitude)),
-//					center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-//				};
-//				const map = new window.kakao.maps.Map(container, options);
-////				const markerPosition = new window.kakao.maps.LatLng(latitude, longitude);
-//				const markerPosition = new window.kakao.maps.LatLng(33.450701, 126.570667);
-//				const marker = new window.kakao.maps.Marker({
-//					position: markerPosition,
-//				});
-//				marker.setMap(map);
-//			});
-//		};
-//		
-//		mapScript.addEventListener("load", onLoadKakaoMap);
-//		
-//		return () => mapScript.removeEventListener("load", onLoadKakaoMap);
-//	}
 
+	const [isOpen, setOpenHandler] = React.useState(false);
 
 	React.useEffect(() => {
-//		const container = document.querySelector('#kakaoMapArea');
-//		const options = {
-////            center: new kakao.maps.LatLng(latitude, longitude),
-//            center: new kakao.maps.LatLng(33.450701, 126.570667),
-//            level: 3
-//        };
-//        
-//		let map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
-		
-		
-	}, [title, name, latitude, longitude, place]);
+		if (open) {
+			setOpenHandler(open);
+		}
+	})
+
+	React.useEffect(() => {
+		console.log("kakao useEffect START");
+		const mapScript = document.createElement("script");
+		mapScript.async = true;
+		mapScript.src = '//dapi.kakao.com/v2/maps/sdk.js?appkey=ab4a5501e93f5f214f48bc77557e3fd2&autoload=false';
+
+		const onLoadKakaoMap = () => {
+			console.log("AAAAAAAAAAAAAAAAA");
+			kakao.maps.load(() => {
+				const container = document.querySelector('#kakaoMapArea');
+
+				console.log("container : " + container);
+
+
+				const options = {
+					//					center: new kakao.maps.LatLng(Number(latitude), Number(longitude)),
+					center: new kakao.maps.LatLng(33.450701, 126.570667),
+				};
+				const map = new kakao.maps.Map(container, options);
+				//				const markerPosition = new kakao.maps.LatLng(latitude, longitude);
+				const markerPosition = new kakao.maps.LatLng(33.450701, 126.570667);
+				const marker = new kakao.maps.Marker({
+					position: markerPosition,
+				});
+				marker.setMap(map);
+			});
+		};
+
+		if (isOpen) {
+			//			mapScript.addEventListener("load", onLoadKakaoMap);
+
+			let container = document.querySelector('#kakaoMapArea');
+			let options = {
+				center: new kakao.maps.LatLng(33.450701, 126.570667),
+				level: 3
+			};
+
+			let map = new kakao.maps.Map(container, options);
+
+			const markerPosition = new kakao.maps.LatLng(33.450701, 126.570667);
+			const marker = new kakao.maps.Marker({
+				position: markerPosition,
+			});
+			marker.setMap(map);
+		}
+		else {
+			mapScript.removeEventListener("load", onLoadKakaoMap);
+		}
+
+	}, [isOpen]);
 
 
 	return (
@@ -104,7 +121,7 @@ const KakaoMap = ({
 					</Typography>
 				</Toolbar>
 			</AppBar>
-			<div className="kakao-map" id="kakaoMapArea" style={{width : 500, height : 500}} />
+			<div className="kakao-map" id="kakaoMapArea" style={{ width: '100%', height: '100%' }} />
 		</Dialog>
 	)
 }
