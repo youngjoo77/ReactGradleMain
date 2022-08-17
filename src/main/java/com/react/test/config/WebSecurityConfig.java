991e608b4f8b381b99233cad8e7ea5530307195b
youngjoo77 <youngjoo.lee@NB-20091105>
2022-08-17 10:19:42 +0900

    commit

--- src/main/java/com/react/test/config/WebSecurityConfig.java
+++ src/main/java/com/react/test/config/WebSecurityConfig.java
@@ -22,46 +22,43 @@ import lombok.RequiredArgsConstructor;
 @EnableWebSecurity
 @Component
 public class WebSecurityConfig {
-    private final TokenProvider tokenProvider;
-    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+	private final TokenProvider tokenProvider;
+	private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+	private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-    	// resource 하위로 권한을 다줘 버림.
-        return (web) -> web.ignoring().antMatchers("/static/**", "/favicon.ico", "/manifest.json", "/**");
-    }
-    
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-    	// request로부터 받은 비밀번호를 암호화
-        return new BCryptPasswordEncoder();
-    }
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		// resource 하위로 권한을 다줘 버림.
+		return (web) -> web.ignoring().antMatchers("/static/**", "/favicon.ico", "/manifest.json", "/**");
+	}
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.httpBasic().disable() // http 사용금지
-        .csrf().disable() // react 에서 token을 localstorage에 저장 하기 위한 설정
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		// request로부터 받은 비밀번호를 암호화
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.httpBasic().disable() // http 사용금지
+				.csrf().disable() // react 에서 token을 localstorage에 저장 하기 위한 설정
 //        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-       
-//        .and()
-        .exceptionHandling()
-        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-        .accessDeniedHandler(jwtAccessDeniedHandler)
-        
-        .and().authorizeRequests()
-        
-        // static resources 권한 설정
-        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-        
-        .antMatchers("/").permitAll()
-        
-        // 서버 권한 설정
-        .antMatchers("/api/auth/**").permitAll() // 토큰 검증 권한설정
-        .anyRequest().authenticated()
-        .and()
-        .apply(new JwtSecurityConfig(tokenProvider));
 
-        return http.build();
-    }
+//        .and()
+				.exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
+				.accessDeniedHandler(jwtAccessDeniedHandler)
+
+				.and().authorizeRequests()
+
+				// static resources 권한 설정
+				.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+
+				.antMatchers("/").permitAll()
+
+				// 서버 권한 설정
+				.antMatchers("/api/auth/**").permitAll() // 토큰 검증 권한설정
+				.anyRequest().authenticated().and().apply(new JwtSecurityConfig(tokenProvider));
+
+		return http.build();
+	}
 }
